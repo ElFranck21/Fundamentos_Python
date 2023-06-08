27c9c1e693800c6a859187671f176c84c88af719
Jesus Trinidad <trinidadjesus117@gmail.com>
2023-06-08 14:32:58 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+# Python es No tipado
+name = "Franck"
+age =18
+is_single =  True
+print(name)
+print(age)
+print(is_single)
+"""
+Hola soy El Franck, tengo 18 años y ya me case alv = True
+"""
+#Utilizando format String
+print("Hola soy El {}, tengo {} años y ya me case alv = {}".format(name, age, is_single))
+print(f"Hola soy El {name}, tengo {age} años y ya me case alv = {is_single}")
