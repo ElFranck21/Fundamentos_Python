0da9b47222687f2dbe1c18577ddd70e66340c526
Jesus Trinidad <trinidadjesus117@gmail.com>
2023-06-14 12:37:30 -0600

    05 Operadores Logicos y booleanos

---
+++ Operadores2.py
@@ -0,0 +1,25 @@
+# Operadores Booleanos
+# < <=
+print(10 < 10)
+print(10 <= 10)
+# > >=
+print(10 > 10)
+print(10 >= 10)
+# ==
+print(10 == 10)
+# !=
+print(10 != 10)
+
+# operadores Logicos
+print("Operadotes logicos")
+# and (true if all false )
+print (True and True)
+print(True and False)
+
+# or (true if at least one true)
+print(False or False)
+print(False or True)
+
+# not true
+print (not True)
+print (not False)
