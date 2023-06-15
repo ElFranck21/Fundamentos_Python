ae58eab3612f48921f44f7a604c3462f212b407a
Jesus Trinidad <trinidadjesus117@gmail.com>
2023-06-15 13:26:44 -0600

    09 Tuplas y Sets

---
+++ tuplas_sets.py
@@ -0,0 +1,29 @@
+# Mas colecciones en Python
+
+
+#tuplas 
+#(item1, item2, item3)
+#listas son mutables 
+# tuplas inmutables
+my_tuple=(1,"dos",3.1,True)
+print(type(my_tuple))
+print( my_tuple)
+print( my_tuple[0])
+print( my_tuple[-1])
+
+#Error
+# my_tuple[0]="algo mas"
+
+# Conjuntos Sets
+# desordenado 
+# mutables
+# no permite duplicados 
+my_set ={"uno","dos","tres","uno"}
+print(type(my_set))
+print(my_set)
+my_set.add(4)
+print(my_set)
+a= {1,2,3,4}
+b={3,4,5,6}
+print(a.union(b))
+print(a.intersection(b))
